717b1ff6209436dbd6131e79affbc4674304ecdd
Mingyuan Huang <2754475494@qq.com>
2020-08-06 19:21:23 +0800

    swagger error

--- hmy_project/src/main/java/com/hmy/web/WebMain.java
+++ hmy_project/src/main/java/com/hmy/web/WebMain.java
@@ -3,10 +3,13 @@ package com.hmy.web;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @MapperScan("com.hmy.web.mapper")
+@ComponentScan(basePackages = {"com.plxcc","com.hmy"})
 public class WebMain
 {
     public static void main(String[] args)
